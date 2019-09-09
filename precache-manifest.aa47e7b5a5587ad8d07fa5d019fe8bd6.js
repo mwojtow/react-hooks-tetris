d8be4e22cc5e915d0deb4493bec3d965891991bc
Marcin Wojtowicz <marcin713@wp.pl>
2019-09-09 15:24:41 +0200

    Updates

---
+++ precache-manifest.aa47e7b5a5587ad8d07fa5d019fe8bd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915d3362a84a9a1a44b71bb699c21f65",
+    "url": "/react-hooks-tetris/index.html"
+  },
+  {
+    "revision": "e3f1f88891ff1cb89e95",
+    "url": "/react-hooks-tetris/static/css/main.cf917ecc.chunk.css"
+  },
+  {
+    "revision": "1b5e3fd9ae77c4172583",
+    "url": "/react-hooks-tetris/static/js/2.f0cf4fc0.chunk.js"
+  },
+  {
+    "revision": "e3f1f88891ff1cb89e95",
+    "url": "/react-hooks-tetris/static/js/main.107e4a38.chunk.js"
+  },
+  {
+    "revision": "e752f37471dbd7cc292e",
+    "url": "/react-hooks-tetris/static/js/runtime~main.bc9530ac.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-hooks-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-hooks-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
